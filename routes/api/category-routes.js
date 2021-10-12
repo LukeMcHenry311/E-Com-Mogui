496e9efe9368c5a264eaa18cb930174dc6d7326c
McHenry <lukemchenry311@gmail.com>
2021-10-12 08:09:58 -0600

    POST, PUT, DELETE categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,10 +42,59 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name,
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: "No category found with this id" });
+        return;
+      }
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: "no post found" });
+        return;
+      }
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
