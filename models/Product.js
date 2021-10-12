aef743848af13f73f33b7f9665e6142330cacf5d
McHenry <lukemchenry311@gmail.com>
2021-10-12 07:58:20 -0600

    product models

--- models/Product.js
+++ models/Product.js
@@ -12,13 +12,24 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
