54323285bcf179ee3132be5412c63ff13e7ea8bd
McHenry <lukemchenry311@gmail.com>
2021-10-12 08:04:15 -0600

    GET category ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,9 +1,46 @@
 const router = require("express").Router();
 const { Category, Product } = require("../../models");
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock"],
+      },
+    ],
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock"],
+      },
+    ],
+  })
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: "No category found with this id" });
+        return;
+      }
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.post("/", (req, res) => {});
 
