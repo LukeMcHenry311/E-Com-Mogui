4347957d8d9f94117c94dee96ece08fad21f1181
McHenry <lukemchenry311@gmail.com>
2021-10-13 01:18:58 -0600

    remove heroku help, it's not deploying anyway

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const Sequelize = require("sequelize");
 
-const sequelize = process.send.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.send
+  ? new Sequelize(process.env)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
